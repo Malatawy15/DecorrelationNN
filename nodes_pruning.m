a5867a7aac12ebde0621d05a4450dfdce1ab5bea
Maged Shalaby <maged918@gmail.com>
2014-11-07 22:36:57 +0200

    removing pruned node

--- nodes_pruning.m
+++ nodes_pruning.m
@@ -1,4 +1,4 @@
-function O = nodes_pruning(out_hidden, error_output, output_size, hidden_size) 
+function O = nodes_pruning(out_hidden, error_output, output_size, hidden_size, w_hidden_input, w_output_hidden) 
 %error output will have to be computed in the function for every i,j, this is just for simplicity
 %both out_hidden and error output are nx1 vectors, where elements in every row are from the different training patterns    
     sum = 0
@@ -18,5 +18,11 @@ function O = nodes_pruning(out_hidden, error_output, output_size, hidden_size)
             min = avg
             minIdx = i
         end
+        
+        %assuming that in w_hidden_input, each row maps to a hidden node
+        w_hidden_input = [w_hidden_input(1:min_idx, 1:end) w_hidden_input(min_idx+1:end, 1:end)]
+        
+        %assuming that in w_output_hidden, each column maps to a hidden node
+        w_output_hidden = [w_output_hidden(1:end, 1:min_idx) w_output_hidden(1:end, min_idx+1, end)]
     end
 end
